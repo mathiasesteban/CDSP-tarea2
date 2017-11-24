3cbe8ce55c33791f6f2eba002c01ab6c9cc18976
Mathias <mathiasesteban.me@gmail.com>
2017-11-24 18:05:08 -0300

    Reescalado a dos

--- src/utils/update_probabilities.cpp
+++ src/utils/update_probabilities.cpp
@@ -6,9 +6,8 @@ using namespace std;
 
 void update_probabilities(PROBS* &probs,unsigned short int &scale, char c){
 
-
+  // Se aumenta la probababilidad del char c
   probs[c-1].high++;
-
   for (unsigned short i = c ; i < 256  ; i++)
   {
     probs[i].low++;
@@ -16,14 +15,37 @@ void update_probabilities(PROBS* &probs,unsigned short int &scale, char c){
   }
   scale++;
 
+  /* La representacion utilizada puede ocasionar overflow, se debe reescalar
+   ************************************************************************ */
 
+  /* Reescalado a 0: reinicializa la estructura
+  *********************************************** */
+  /*
   if (scale == MAXIMUM_SCALE){
     probs = initialize_probabilities(256);
     scale = 256;
   }
+  */
 
-  /*cout << "The following character \""
-  		<< c << "\" updated its probability to "
-  		<< probs[c-1].high - probs[c-1].low
-  		<< "/" << scale << "\n";*/
+  /* Reescalado a dos: reescala todas los intervalos entre 2
+  ********************************************************** */
+  if (scale == MAXIMUM_SCALE){
+
+    probs[0].low = probs[0].low / 2 ;
+    probs[0].high = probs[0].high / 2;
+
+    if (probs[0].high == 0)
+      probs[0].high = 1;
+
+    for (unsigned short i = 1 ; i < 256  ; i++)
+    {
+      probs[i].low = probs[i-1].high;
+      probs[i].high = probs[i].high / 2;
+
+      if(probs[i].high <= probs[i].low)
+        probs[i].high = probs[i].low +1 ;
+    }
+    scale = probs[255].high;
+
+  }
 }
